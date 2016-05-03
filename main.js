3d53975906a2461644c9120132c9c7ea755ac2c8
Corinne Winthrop <corinne.winthrop@gmail.com>
2016-05-02 19:45:31 -0600

    Finish hw in class

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
 $(document).ready(function(){
 
 	$.ajax({
-		url: 'https://api.myjson.com/bins/1kmxk',
+		url: 'https://api.myjson.com/bins/274f8',
 		type: 'GET',
 		data: {
 			format: 'json'
@@ -14,8 +15,43 @@ $(document).ready(function(){
 			for(i=0; i < states.length; i++) {
 				var firstName = states[i].name;
 				var cap = states[i].capital;
-				$('#states').append("<h3>" + firstName + "</h3>");
+				var entered = states[i].enteredUnion;
+				var tree = states[i].symbols[0].tree;
+				var flag = states[i].symbols[1].flag;
+				$('#states').append("<h3>" + firstName + "</h3><h4>" + cap + " - " + entered + " - " + tree + "</h4>");
+				$('#states').append("<img src='"+ flag + "'></img>")
+				$('#states').append("<span id='pics'>" + "</span>");
+				document.getElementById("pics").id = firstName;
+				$('#states').append("<span id='bird'>" + "</span>");
+				document.getElementById("bird").id = states[i].symbols[0].bird.split(" ")[0];
+				afterLoad();
 			}
 		}
 	})
+
+
+	function afterLoad() {
+		$("#states h3").css("color", "navy")
+	};
+
+	function clickPic(){
+		$("#states").on('click', 'h3', function(){
+			var stateName = $(this).text()
+			console.log(stateName)
+			$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+				{
+					api_key: "4bc9fb038f6eaa557f946ead1eef1bd5",
+					tags: "sunset, " + stateName,
+					format: "json"
+				},
+				function(data) {
+					$.each(data.items, function(i,item){
+						$("<img />").attr("src", item.media.m).appendTo("#states #" + stateName);
+						if ( i == 3 ) return false;
+				});
+			});
+		});
+	}
+
+	clickPic();
 });
